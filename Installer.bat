2ccd17b783d1e5f0b7255636545c87a9fc82daf0
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-06-19 17:30:32 +0200

    Add files via upload

--- Installer.bat
+++ Installer.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell wget https://github.com/SaoasBlubb/Multi-Downloader/releases/download/v1.0.0/YouTube_Spotify_Downloader.zip -O YouTube_Spotify_Downloader.zip 2>&1>nul
+wget -F -q https://github.com/SaoasBlubb/Multi-Downloader/releases/download/v1.0.0/YouTube_Spotify_Downloader.zip -O YouTube_Spotify_Downloader.zip 2>&1>nul
 tar -xf YouTube_Spotify_Downloader.zip 2>&1>nul
 del YouTube_Spotify_Downloader.zip 2>&1>nul
 cd YouTube_Spotify_Downloader 2>&1>nul

51ea858ac650fc34dcf737ad209826140adb6ccf
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-09-23 09:25:44 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,5 +1,7 @@
 @echo off
 
+mkdir libs
+cd libs
 winget install -e --id Python.Python.3.11
 winget install -e --id Git.Git
 winget install -e --id JernejSimoncic.Wget
@@ -7,6 +9,6 @@ winget install -e --id cURL.cURL
 winget install yt-dlp
 winget install -e --id Gyan.FFmpeg
 pip install spotdl
-
+cd ..
 start setup.exe 2>&1>nul
 exit

8033df022d21f240e4be347b478d8216e5c26649
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-09-23 08:19:53 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -3,5 +3,9 @@ wget -F -q https://github.com/SaoasBlubb/Multi-Downloader/releases/download/v2.0
 tar -xf YouTube_Spotify_Downloader.zip 2>&1>nul
 del YouTube_Spotify_Downloader.zip 2>&1>nul
 cd YouTube_Spotify_Downloader 2>&1>nul
+mkdir libs 2>&1>nul
+cd libs 2>&1>nul
+git clone https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe 2>&1>nul
+
 start setup.exe 2>&1>nul
 exit

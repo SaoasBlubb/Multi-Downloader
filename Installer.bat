c53b7d9238dc9a7333e5b5eddc09b8c7468b5298
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-09-23 09:13:42 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,11 +1,11 @@
 @echo off
-wget -F -q https://github.com/SaoasBlubb/Multi-Downloader/releases/download/v2.0.0/YouTube_Spotify_Downloader.zip -O YouTube_Spotify_Downloader.zip 2>&1>nul
-tar -xf YouTube_Spotify_Downloader.zip 2>&1>nul
-del YouTube_Spotify_Downloader.zip 2>&1>nul
-cd YouTube_Spotify_Downloader 2>&1>nul
-mkdir libs 2>&1>nul
-cd libs 2>&1>nul
-git clone https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe 2>&1>nul
+
+winget install -e --id Python.Python.3.11
+winget install -e --id Git.Git
+winget install -e --id JernejSimoncic.Wget
+winget install -e --id cURL.cURL
+winget install yt-dlp
+
 
 start setup.exe 2>&1>nul
 exit

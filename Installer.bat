77795f6f1b3e6e7f1ba9c321a0dc2b960a6cdfa2
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-09-25 18:08:35 +0200

    Add files via upload

--- Installer.bat
+++ Installer.bat
@@ -1,11 +1,8 @@
 @echo off
-powershell curl https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -o wget.exe
-REM wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/setup.exe
-REM wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/update.exe
-REM wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/YouTube_Spotify_Downloader.exe
+powershell curl https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O wget.exe
 mkdir lib
 cd lib
-powershell curl https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -o wget.exe
+powershell curl https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O wget.exe
 wget -F -q --no-hsts https://github.com/spotDL/spotify-downloader/releases/latest/download/spotdl-4.2.1-win32.exe -O spotdl.exe
 wget -F -q --no-hsts https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -O yt-dlp.exe
 wget -F -q --no-hsts https://github.com/BtbN/FFmpeg-Builds/releases/latest/download/ffmpeg-master-latest-win64-gpl.zip -O ffmpeg-master-latest-win64-gpl.zip

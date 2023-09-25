146c378d3adc5afcc2ee79b4009c65e64140fc63
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-09-25 17:42:29 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,26 +1,18 @@
 @echo off
-
 powershell curl https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -o wget.exe
-
-wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/setup.exe
-wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/update.exe
-wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/YouTube_Spotify_Downloader.exe
-
+REM wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/setup.exe
+REM wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/update.exe
+REM wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/YouTube_Spotify_Downloader.exe
 mkdir lib
 cd lib
-
 powershell curl https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -o wget.exe
-
-wget -F -q --no-hsts https://github.com/spotDL/spotify-downloader/releases/latest/download/spotdl-4.2.1-win32.exe -o spotdl.exe
-wget -F -q --no-hsts https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -o yt-dlp.exe
-wget -F -q --no-hsts https://github.com/BtbN/FFmpeg-Builds/releases/latest/download/ffmpeg-master-latest-win64-gpl.zip -o ffmpeg.zip
-tar -xf ffmpeg.zip
-del ffmpeg.zip
+wget -F -q --no-hsts https://github.com/spotDL/spotify-downloader/releases/latest/download/spotdl-4.2.1-win32.exe -O spotdl.exe
+wget -F -q --no-hsts https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -O yt-dlp.exe
+wget -F -q --no-hsts https://github.com/BtbN/FFmpeg-Builds/releases/latest/download/ffmpeg-master-latest-win64-gpl.zip -O ffmpeg-master-latest-win64-gpl.zip
+tar -xf ffmpeg-master-latest-win64-gpl.zip
+del ffmpeg-master-latest-win64-gpl.zip
 cd ffmpeg-master-latest-win64-gpl/bin
 call xcopy * ..\.. /s /e /Y
-cd ..\..
 rmdir /s /q ffmpeg-master-latest-win64-gpl
-mkdir C:\Saoas
-copy * C:\Saoas
 start setup.exe
 exit

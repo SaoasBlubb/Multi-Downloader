2ed7c94072d90c4db0575560b8eb979e64194808
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-09-25 18:02:39 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
-REM mkdir "C:\Saoas"
-REM copy "lib\*.exe" "C:\Saoas"
+mkdir "C:\Saoas"
+copy "lib\*.exe" "C:\Saoas"
 cd "C:\Saoas"
 setx PATH "C:\Saoas;%PATH%"
 REM start %~dp0\YouTube_Spotify_Downloader.exe 

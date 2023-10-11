56b05a5bc060c23046fc31ffb8f1840a857f064f
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2023-10-11 10:13:12 +0200

    Add files via upload

--- update.bat
+++ update.bat
@@ -2,6 +2,6 @@
 title Updating Files...
 taskkill /IM Multi_Downloader.exe 2>&1>nul
 del Multi_Downloader.exe 2>&1>nul
-ren Multi_Downloader.exe.new Multi_Downloader.exe 2>&1>nul
+cmd /c ren Multi_Downloader.exe.new Multi_Downloader.exe 2>&1>nul
 Multi_Downloader.exe 2>&1>nul
 exit

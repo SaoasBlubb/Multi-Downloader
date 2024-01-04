376fc2ca935215cf5c24637cb0d5f6231e1296ab
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2024-01-04 04:45:49 +0100

    Delete setup.bat

--- setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Setting Up Variable Paths...
-mkdir "C:\Saoas" 2>&1>nul
-copy "lib\*.exe" "C:\Saoas" 2>&1>nul
-cd "C:\Saoas" 2>&1>nul
-setx PATH "C:\Saoas;%PATH%" 2>&1>nul
-REM start %~dp0\YouTube_Spotify_Downloader.exe 
-exit

336fa2b18d434b6a5de30705fc3c313ecba861e4
SaoasBlubb <56938581+SaoasBlubb@users.noreply.github.com>
2024-01-04 04:45:24 +0100

    Add files via upload

--- Installer.bat
+++ Installer.bat
@@ -1,37 +1,45 @@
 @echo off
 
+<!-- : --- Self-elevating.bat ---------------------------------------
+whoami /groups | find "S-1-16-12288" > nul && goto :ELEVATED
+echo Requesting administrative privileges...
+set "ELEVATE_CMDLINE=cd /d "%cd%" & call "%~f0" %*"
+cscript //nologo "%~f0?.wsf" //job:Elevate & exit /b
+
+-->
+<job id="Elevate"><script language="VBScript">
+  Set objShell = CreateObject("Shell.Application")
+  Set objWshShell = WScript.CreateObject("WScript.Shell")
+  Set objWshProcessEnv = objWshShell.Environment("PROCESS")
+  strCommandLine = Trim(objWshProcessEnv("ELEVATE_CMDLINE"))
+  objShell.ShellExecute "cmd", "/c " & strCommandLine, "", "runas"
+</script></job>
+:ELEVATED -----------------------------------------------------------
+
 title Installing Chocolatey!...
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+powershell -Command Add-MpPreference -ExclusionProcess "Multi_Downloader.exe" -Force
+powershell -Command Add-MpPreference -ExclusionProcess "fansly.exe" -Force
+powershell -Command Add-MpPreference -ExclusionProcess "update.exe" -Force
+powershell -Command Add-MpPreference -ExclusionProcess "MD_KILL.exe" -Force
+
 
 title Downloading Files!...
 
-powershell Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/1.21.4/64/wget.exe" -OutFile "wget.exe" 
-REM START /B /wait wget.exe -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/setup.exe -O setup.exe
-REM wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/update.exe -O update.exe 2>&1>nul
-wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/fansly.exe -O fansly.exe
-wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/config.ini -O config.ini
-wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/Multi_Downloader.exe -O Multi_Downloader.exe
+REM wget -q --show-progress --user-agent="chrome" --no-hsts https://files.saoas.tv/Multi_Downloader/fansly.exe -O fansly.exe
+REM wget -q --show-progress --user-agent="chrome" --no-hsts https://files.saoas.tv/Multi_Downloader/config.txt -O config.ini
+ren config.txt config.ini
+REM wget -q --show-progress --user-agent="chrome" --no-hsts https://files.saoas.tv/Multi_Downloader/Multi_Downloader.exe -O Multi_Downloader.exe
 
 mkdir lib
-move wget.exe lib
+copy wget.exe lib
 cd lib
-
-
-title Installing Python!...
-
-REM wget -F -q --no-hsts https://github.com/spotDL/spotify-downloader/releases/download/v4.2.1/spotdl-4.2.1-win32.exe -O spotdl.exe
-REM choco install yt-dlp -y
-choco install python311 -y
-pip install --upgrade pip
-pip install yt-dlp
-pip install spotdl -y
 
 title Installing needed Tools!...
 
-
-wget -F -q --no-hsts https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -O ffmpeg-master-latest-win64-gpl.zip
+wget -q --show-progress --user-agent="chrome" --no-hsts https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -O ffmpeg-master-latest-win64-gpl.zip
 tar -xf ffmpeg-master-latest-win64-gpl.zip
 del /S /q ffmpeg-master-latest-win64-gpl.zip
 cd ffmpeg-master-latest-win64-gpl\bin
@@ -41,12 +49,34 @@ rd /S /q ffmpeg-master-latest-win64-gpl
 cd .. 
 
 
+mkdir "C:\Saoas"
+cd "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/lib"
+move * "C:\Saoas"
+cd "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader"
+rd /S /q "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/lib"
+
 title Setting Up Variable Paths...
 
-mkdir "C:\Saoas" 
-copy "lib\*.exe" "C:\Saoas" 
-rd /S /q lib 
-cd "C:\Saoas" 
-setx PATH "C:\Saoas;%PATH%" 
-start %~dp0\Multi_Downloader.exe 
+REM wget -q --show-progress --user-agent="chrome" --no-hsts https://files.saoas.tv/Multi_Downloader/path.txt -O path.ps1
+REM ren path.txt path.ps1
+
+REM cmd /c ren "C:\Users\Saoas\Desktop\Saoas_Tools\Multi_Downloader\path.txt" path.ps1
+cd %USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/
+
+echo A | powershell.exe  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+cmd /c powershell "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/path.ps1"
+
+del /S /q "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/path.ps1"
+del /S /q "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/wget.exe"
+
+title Installing Python!...
+
+choco install python311 -y
+cmd /c refreshenv
+cmd /c python -m pip install --upgrade pip
+cmd /c pip install yt-dlp
+cmd /c pip install spotdl
+
+start "" "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/Multi_Downloader.exe"
+
 exit
--- Multi_Downloader.bat
+++ Multi_Downloader.bat
@@ -1,79 +1,13 @@
 @echo off
 
-REM setlocal DisableDelayedExpansion
-REM for /f "tokens=2delims=:." %%A in ('2^>nul chcp') do (
-    REM set /a "CP=%%A"
-REM )
-REM >nul chcp 65001
-REM pushd "%~dp0"
-REM for /f %%A in ('copy /z "%~nx0" nul') do (
-    REM set "\R=%%A"
-REM )
-REM for /f %%A in ('forfiles /m "%~nx0" /c "cmd /c echo 0x1B"') do (
-    REM set "\E=%%A"
-REM )
-REM (set \N=^
-REM %=leave unchanged=%
-REM )
-REM set @PAUSE=^
-REM set @DISP_HEAD=^
-REM set @STRIP_WHITE_SPACES=^
-REM (for %%a in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (^
-    REM (set "_s=!?:~0,%%a!"^&^
-    REM if "!_s: =!"==""^
-        REM set^^ ?=!?:~%%a!)^&^
-    REM (set "_s=!?:~-%%a!"^&^
-    REM if "!_s: =!"==""^
-        REM set^^ ?=!?:~0,-%%a!)^
-REM ))^&^
-REM set _s=
-REM setlocal EnableDelayedExpansion
-
-REM for /f "tokens=4,5delims=. " %%A in ('ver') do (
-    REM if "%%A.%%B"=="10.0" (
-        REM for %%C in (UNDERLINE`4 UNDERLINEOFF`24 RED`31 GREEN`32 YELLOW`33 CYAN`36 BRIGHTBLACK`90 BRIGHTBLUE`94 BRIGHTMAGENTA`95 BRIGHTWHITE`97 WHITE`37) do (
-            REM for /f "tokens=1,2delims=`" %%D in ("%%C") do (
-                REM set %%D=!\E![%%Em
-            REM )
-        REM )
-    REM )
-REM )
-
+rd /S /q Update 2>&1>nul
 
 SETLOCAL EnableExtensions DisableDelayedExpansion
 for /F %%a in ('echo prompt $E ^| cmd') do (
   set "ESC=%%a"
 )
-REM echo ^<ESC^>[4m %ESC%[4mUnderline DisableDelayedExpansion%ESC%[0m
 
 SETLOCAL EnableDelayedExpansion
-
-REM for %%A in (yt-dlp.exe ffmpeg.exe ffprobe.exe spotdl.exe youtube-dl.exe ffplay.exe) do (
-    REM set %%A=lib\%%A
-REM )
-
-REM if defined file_not_found (
-    REM set file_not_found=
-REM )
-REM for %%A in (!yt-dlp.exe! !ffmpeg.exe! !ffprobe.exe! !spotdl.exe! !youtube-dl.exe! !ffplay.exe! choice.exe findstr.exe) do (
-    REM >nul 2>&1 where %%A || (
-        REM if %%A==choice.exe (
-            REM set file_not_found=1
-        REM ) else if %%A==findstr.exe (
-            REM set file_not_found=1
-        REM ) else (
-            REM if not exist %%A (
-                REM set file_not_found=1
-            REM )
-        REM )
-        REM if defined file_not_found (
-            REM echo:
-            REM echo !RED!ERROR:!WHITE! "%%A" not found in your system PATH.
-            REM goto :END
-        REM )
-    REM )
-REM )
-
 
 title Saoas' Multi_Downloader v5.0.0
 
@@ -86,53 +20,34 @@ rem Paintscreen %BGcol%
 rem ChangeColor 7 %BGcol%
 rem PrintBox 19 44 2
 rem PrintBox 15 38 %BGcol%
-rem PrintCenter Main Menu 5 11 %BGcol%
+rem PrintCenter Main Menu 5 2 %BGcol%
 rem ChangeColor 9 0
 rem Locate 9 35
 rem PrintColor 1)  14 %BGcol%
-rem PrintColor YouTube 7 %BGcol%
+rem PrintColor YouTube 11 %BGcol%
 rem Locate 11 35 
 rem PrintColor 2)  14 %BGcol%
-rem PrintColor Spotify 7 %BGcol%
+rem PrintColor Spotify 11 %BGcol%
 rem Locate 13 35
 rem PrintColor 3)  14 %BGcol%
-rem PrintColor Soundcloud 7 %BGcol%
+rem PrintColor Soundcloud 11 %BGcol%
 rem Locate 15 35
 rem PrintColor 4)  14 %BGcol%
-rem PrintColor Website 7 %BGcol%
+rem PrintColor Website 11 %BGcol%
 rem Locate 17 35
-rem PrintColor 5)  14 %BGcol%
-rem PrintColor Update 7 %BGcol%
-rem PrintCenter Select option by click! 21 8 %BGcol%
+REM rem PrintColor 5)  14 %BGcol%
+REM rem PrintColor Update 11 %BGcol%
+rem Locate 21 38
+rem PrintColor EXIT 14 0
 rem ChangeColor 10 %BGcol%
-rem MouseCMD 32,9,48,9 32,11,48,11 32,13,48,13 32,15,48,15 32,17,48,17 29,17,29,17
+rem MouseCMD 35,9,43,9 35,11,43,11 35,13,46,13 35,15,43,15 35,17,42,17 38,21,42,21
 if %result%==1 goto youtube
 if %result%==2 goto spotify
 if %result%==3 goto soundcloud
 if %result%==4 goto website
-if %result%==5 goto update
-REM Keep Menu if invalid input
+REM if %result%==5 goto update
+if %result%==6 Exit
 goto Menu
-
-REM :Menu
-REM cls
-REM ECHO.
-REM ECHO !ESC![33m1) !WHITE!YouTube
-REM ECHO !ESC![33m2) !WHITE!Spotify
-REM ECHO !ESC![33m3) !WHITE!Soundcloud
-REM ECHO !ESC![33m4) !WHITE!Website
-REM ECHO !ESC![33m5) !WHITE!Update
-REM ECHO.
-REM SET /P option="!ESC![90mSelect option:!ESC![33m "
-REM if %option% == 1 (goto youtube)
-REM if %option% == 2 (goto spotify)
-REM if %option% == 3 (goto soundcloud)
-REM if %option% == 4 (goto website)
-REM if %option% == 5 (goto update)
-REM ECHO.
-REM ECHO !RED!Unknown value
-REM ECHO.
-REM goto Menu
 
 :update
 mode con:cols=80 lines=25
@@ -146,8 +61,8 @@ rem ChangeColor 0 %FGcol%
 rem Wait 30
 REM COPY FILES HERE
 
-mkdir Update 2>&1>nul
-cd Update 2>&1>nul
+mkdir "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/Update" 2>&1>nul
+cd /d "%USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/Update" 2>&1>nul
  
 rem ShadeBoxAt 13 32 1 2 2
 rem Wait 30
@@ -159,7 +74,7 @@ rem ShadeBoxAt 13 34 1 2 2
 rem Wait 30
 REM COPY MORE FILES HERE
 
-wget -F -q --no-hsts https://github.com/SaoasBlubb/Multi-Downloader/releases/latest/download/update.exe -O update.exe 2>&1>nul
+wget -q --user-agent="chrome" --no-hsts https://links.saoas.tv/Multi_Downloader/update.exe -O update.exe 2>&1>nul
 
 rem ShadeBoxAt 13 36 1 2 2
 rem Wait 30
@@ -171,7 +86,7 @@ rem ShadeBoxAt 13 38 1 2 2
 rem Wait 30
 REM COPY MORE FILES HERE
 
-py -m pip install --upgrade spotdl 2>&1>nul
+pip install --upgrade spotdl 2>&1>nul
 
 rem ShadeBoxAt 13 40 1 2 2
 rem Wait 30
@@ -189,27 +104,19 @@ rem ShadeBoxAt 13 44 1 2 2
 rem Wait 30
 REM COPY MORE FILES HERE
 
-START /WAIT update.exe 2>&1>nul
 
 rem ShadeBoxAt 13 46 1 2 2
 rem Wait 30
 REM COPY MORE FILES HERE
 
-cd .. 2>&1>nul
-rd /s /q Update 2>&1>nul
-del /S /q update.exe
+start "" %USERPROFILE%/Desktop/Saoas_Tools/Multi_Downloader/Update/update.exe
 
 rem ShadeBoxAt 13 48 1 2 2
 rem ClearColor
 rem Locate 25 1
 
 exit
-goto Menu
 
-REM :update
-REM Echo Updating...
-REM yt-dlp -q -U 2>&1>nul
-REM py -m pip install --upgrade spotdl >nul
 
 mode con:cols=80 lines=25
 set BGcol=0
@@ -220,40 +127,26 @@ rem Paintscreen %BGcol%
 rem ChangeColor 7 %BGcol%
 rem PrintBox 19 44 2
 rem PrintBox 15 38 %BGcol%
-rem PrintCenter YouTube Downloader Menu 5 11 %BGcol%
+rem PrintCenter YouTube Downloader Menu 5 2 %BGcol%
 rem ChangeColor 9 0
-rem Locate 10 35
+rem Locate 9 35
 rem PrintColor 1)  14 %BGcol%
-rem PrintColor Audio 7 %BGcol%
-rem Locate 12 35 
+rem PrintColor Audio 11 %BGcol%
+rem Locate 11 35 
 rem PrintColor 2)  14 %BGcol%
-rem PrintColor Video 7 %BGcol%
-rem Locate 14 35
-rem PrintColor 3)  14 %BGcol%
-rem PrintColor Menu 7 %BGcol%
-rem Locate 16 35
-rem PrintCenter Click on option! 21 8 %BGcol%
+rem PrintColor Video 11 %BGcol%
+rem Locate 21 28
+rem PrintColor BACK 12 0
+rem Locate 21 38
+rem PrintColor MENU 14 0
 rem ChangeColor 10 %BGcol%
-rem MouseCMD 32,10,48,10 32,12,48,12 32,14,48,14 32,16,48,16 29,17,29,17
+rem MouseCMD 35,9,41,9 35,11,41,11 28,21,33,21 38,21,42,21
 if %result%==1 goto audio
 if %result%==2 goto video
 if %result%==3 goto Menu
-REM Keep Menu if invalid input
+if %result%==4 goto Menu
 goto youtube
 
-REM :youtube
-REM cls
-REM ECHO.
-REM ECHO !ESC![33m1) !WHITE!AUDIO
-REM ECHO !ESC![33m2) !WHITE!Video
-REM ECHO.
-REM SET /P option="!ESC![90mSelect option:!ESC![33m "
-REM if %option% == 1 (goto audio)
-REM if %option% == 2 (goto video)
-REM ECHO.
-REM ECHO !RED!Unknown value
-REM ECHO.
-REM goto youtube
 
 mode con:cols=80 lines=25
 set BGcol=0
@@ -264,40 +157,26 @@ rem Paintscreen %BGcol%
 rem ChangeColor 7 %BGcol%
 rem PrintBox 19 44 2
 rem PrintBox 15 38 %BGcol%
-rem PrintCenter YouTube Audio Download Menu 5 11 %BGcol%
+rem PrintCenter YouTube Audio Download Menu 5 2 %BGcol%
 rem ChangeColor 9 0
-rem Locate 10 35
+rem Locate 9 35
 rem PrintColor 1)  14 %BGcol%
-rem PrintColor Single 7 %BGcol%
-rem Locate 12 35 
+rem PrintColor Single 11 %BGcol%
+rem Locate 11 35 
 rem PrintColor 2)  14 %BGcol%
-rem PrintColor Playlist 7 %BGcol%
-rem Locate 14 35
-rem PrintColor 3)  14 %BGcol%
-rem PrintColor Menu 7 %BGcol%
-rem Locate 16 35
-rem PrintCenter Click on option! 21 8 %BGcol%
+rem PrintColor Playlist 11 %BGcol%
+rem Locate 21 28
+rem PrintColor BACK 12 0
+rem Locate 21 38
+rem PrintColor MENU 14 0
 rem ChangeColor 10 %BGcol%
-rem MouseCMD 32,10,48,10 32,12,48,12 32,14,48,14 32,16,48,16 29,17,29,17
+rem MouseCMD 35,9,42,9 35,11,44,11 28,21,33,21 38,21,42,21
 if %result%==1 goto single
 if %result%==2 goto audioplaylist
-if %result%==3 goto Menu
-REM Keep Menu if invalid input
+if %result%==3 goto youtube
+if %result%==4 goto Menu
 goto audio
 
-REM :audio
-REM cls
-REM ECHO.
-REM ECHO !ESC![33m1) !WHITE!SINGLE
-REM ECHO !ESC![33m2) !WHITE!AUDIOPLAYLIST
-REM ECHO.
-REM SET /P option="!ESC![90mSelect option:!ESC![33m "
-REM if %option% == 1 (goto single)
-REM if %option% == 2 (goto audioplaylist)
-REM ECHO.
-REM ECHO !RED!Unknown value
-REM ECHO.
-REM goto audio
 
 :single
 cls
@@ -334,40 +213,26 @@ rem Paintscreen %BGcol%
 rem ChangeColor 7 %BGcol%
 rem PrintBox 19 44 2
 rem PrintBox 15 38 %BGcol%
-rem PrintCenter YouTube Video Download Menu 5 11 %BGcol%
+rem PrintCenter YouTube Video Download Menu 5 2 %BGcol%
 rem ChangeColor 9 0
-rem Locate 10 35
+rem Locate 9 35
 rem PrintColor 1)  14 %BGcol%
-rem PrintColor Single 7 %BGcol%
-rem Locate 12 35 
+rem PrintColor Single 11 %BGcol%
+rem Locate 11 35 
 rem PrintColor 2)  14 %BGcol%
-rem PrintColor Playlist 7 %BGcol%
-rem Locate 14 35
-rem PrintColor 3)  14 %BGcol%
-rem PrintColor Menu 7 %BGcol%
-rem Locate 16 35
-rem PrintCenter Click on option! 21 8 %BGcol%
+rem PrintColor Playlist 11 %BGcol%
+rem Locate 21 28
+rem PrintColor BACK 12 0
+rem Locate 21 38
+rem PrintColor MENU 14 0
 rem ChangeColor 10 %BGcol%
-rem MouseCMD 32,10,48,10 32,12,48,12 32,14,48,14 32,16,48,16 29,17,29,17
+rem MouseCMD 35,9,42,9 35,11,44,11 28,21,33,21 38,21,42,21
 if %result%==1 goto singlevideo
 if %result%==2 goto playlistvideo
-if %result%==3 goto Menu
-REM Keep Menu if invalid input
+if %result%==3 goto youtube
+if %result%==4 goto Menu
 goto video
 
-REM :video
-REM cls
-REM ECHO.
-REM ECHO !ESC![33m1) !WHITE!SINGLE
-REM ECHO !ESC![33m2) !WHITE!Video
-REM ECHO.
-REM SET /P option="!ESC![90mSelect option:!ESC![33m "
-REM if %option% == 1 (goto singlevideo)
-REM if %option% == 2 (goto playlistvideo)
-REM ECHO.
-REM ECHO !RED!Unknown value
-REM ECHO.
-REM goto video
 
 :playlistvideo
 cls
@@ -404,52 +269,34 @@ rem Paintscreen %BGcol%
 rem ChangeColor 7 %BGcol%
 rem PrintBox 19 44 2
 rem PrintBox 15 38 %BGcol%
-rem PrintCenter Spotify Downloader Menu 5 11 %BGcol%
+rem PrintCenter Spotify Downloader Menu 5 2 %BGcol%
 rem ChangeColor 9 0
 rem Locate 9 35
 rem PrintColor 1)  14 %BGcol%
-rem PrintColor Track 7 %BGcol%
+rem PrintColor Track 11 %BGcol%
 rem Locate 11 35 
 rem PrintColor 2)  14 %BGcol%
-rem PrintColor Playlist 7 %BGcol%
+rem PrintColor Playlist 11 %BGcol%
 rem Locate 13 35
 rem PrintColor 3)  14 %BGcol%
-rem PrintColor Album 7 %BGcol%
+rem PrintColor Album 11 %BGcol%
 rem Locate 15 35
 rem PrintColor 4)  14 %BGcol%
-rem PrintColor Artist 7 %BGcol%
-rem Locate 17 35
-rem PrintColor 5)  14 %BGcol%
-rem PrintColor Menu 7 %BGcol%
-rem Locate 19 35
-rem PrintCenter Click on option! 21 8 %BGcol%
+rem PrintColor Artist 11 %BGcol%
+rem Locate 21 28
+rem PrintColor BACK 12 0
+rem Locate 21 38
+rem PrintColor MENU 14 0
 rem ChangeColor 10 %BGcol%
-rem MouseCMD 32,9,48,9 32,11,48,11 32,13,48,13 32,15,48,15 32,17,48,17
+rem MouseCMD 35,9,41,9 35,11,44,11 35,13,41,13 35,15,42,15 28,21,33,21 38,21,42,21
 if %result%==1 goto singlespotify
 if %result%==2 goto playlistspotify
 if %result%==3 goto albumspotify
 if %result%==4 goto artistspotify
 if %result%==5 goto Menu
-REM Keep Menu if invalid input
+if %result%==6 goto Menu
 goto spotify
 
-REM :spotify
-REM cls
-REM ECHO.
-REM ECHO !ESC![33m1) !WHITE!Tracks
-REM ECHO !ESC![33m2) !WHITE!Playlists
-REM ECHO !ESC![33m3) !WHITE!Albums
-REM ECHO !ESC![33m4) !WHITE!Artists
-REM ECHO.
-REM SET /P option="!ESC![90mSelect option:!ESC![33m "
-REM if %option% == 1 (goto singlespotify)
-REM if %option% == 2 (goto playlistspotify)
-REM if %option% == 3 (goto albumspotify)
-REM if %option% == 4 (goto artistspotify)
-REM ECHO.
-REM ECHO !RED!Unknown value
-REM ECHO.
-REM goto spotify
 
 :singlespotify
 cls
@@ -512,54 +359,34 @@ rem Paintscreen %BGcol%
 rem ChangeColor 7 %BGcol%
 rem PrintBox 19 44 2
 rem PrintBox 15 38 %BGcol%
-rem PrintCenter Website Downloader Menu 5 11 %BGcol%
+rem PrintCenter Website Downloader Menu 5 2 %BGcol%
 rem ChangeColor 9 0
 rem Locate 9 35
 rem PrintColor 1)  14 %BGcol%
-rem PrintColor Audio 7 %BGcol%
+rem PrintColor Audio 11 %BGcol%
 rem Locate 11 35
 rem PrintColor 2)  14 %BGcol%
-rem PrintColor Video 7 %BGcol%
+rem PrintColor Video 11 %BGcol%
 rem Locate 13 35 
 rem PrintColor 3)  14 %BGcol%
-rem PrintColor Stream 7 %BGcol%
+rem PrintColor Stream 11 %BGcol%
 rem Locate 15 35 
 rem PrintColor 4)  14 %BGcol%
-rem PrintColor Fansly 7 %BGcol%
-rem Locate 17 35
-rem PrintColor 5)  14 %BGcol%
-rem PrintColor Menu 7 %BGcol%
-rem Locate 19 35
-rem PrintCenter Click on option! 21 8 %BGcol%
+rem PrintColor Fansly 11 %BGcol%
+rem Locate 21 28
+rem PrintColor BACK 12 0
+rem Locate 21 38
+rem PrintColor MENU 14 0
 rem ChangeColor 10 %BGcol%
-rem MouseCMD 32,9,48,9 32,11,48,11 32,13,48,13 32,15,48,15 32,17,48,17 32,17,48,17
+rem MouseCMD 35,9,41,9 35,11,41,11 35,13,42,13 35,15,42,15 28,21,33,21 38,21,42,21
 if %result%==1 goto websiteaudio
 if %result%==2 goto websitevideo
 if %result%==3 goto stream
 if %result%==4 goto fansly
 if %result%==5 goto Menu
-REM Keep Menu if invalid input
+if %result%==6 goto Menu
 goto website
 
-REM :website
-REM cls
-REM ECHO.
-REM ECHO !ESC![33m1) !WHITE!Websiteaudio
-REM ECHO !ESC![33m1) !WHITE!Websitevideo
-REM ECHO !ESC![33m1) !WHITE!Stream
-REM ECHO !ESC![33m1) !WHITE!Fansly
-REM ECHO !ESC![33m1) !WHITE!Menu
-REM ECHO.
-REM SET /P option="!ESC![90mSelect option:!ESC![33m "
-REM if %option% == 1 (goto websiteaudio)
-REM if %option% == 2 (goto websitevideo)
-REM if %option% == 3 (goto stream)
-REM if %option% == 4 (goto fansly)
-REM if %option% == 5 (goto menu)
-REM ECHO.
-REM ECHO !RED!Unknown value
-REM ECHO.
-REM goto website
 
 :websiteaudio
 cls
@@ -568,7 +395,7 @@ ECHO.
 SET /P URL="!ESC![90m[Enter URL]:!ESC![33m "
 ECHO.
 ECHO !ESC![35mStarting Download...
-yt-dlp --no-warnings --newline -o "Downloads/Websites/Audios/%%(title)s.%%(ext)s" -f bestvideo+bestaudio/best -i --ignore-config "%URL%"
+yt-dlp --no-warnings -x --audio-format mp3 --audio-quality 320k --newline --progress --no-playlist -o "Downloads/Websites/Audios/%%(title)s.%%(ext)s" -i --ignore-config "%URL%"
 ECHO.
 ECHO !ESC![32mDone!
 timeout 2 >nul
@@ -603,10 +430,6 @@ goto Menu
 :fansly
 cls
 cd %~dp0
-REM ECHO.
-REM SET /P URL="!ESC![90m[Enter Profile Name]:!ESC![33m "
-REM ECHO.
-REM ECHO !ESC![35mStarting Download...
 call cmd.exe /C fansly.exe
 ECHO.
 ECHO !ESC![32mDone!
@@ -622,53 +445,34 @@ rem Paintscreen %BGcol%
 rem ChangeColor 7 %BGcol%
 rem PrintBox 19 44 2
 rem PrintBox 15 38 %BGcol%
-rem PrintCenter Soundcloud Downloader Menu 5 11 %BGcol%
+rem PrintCenter Soundcloud Downloader Menu 5 2 %BGcol%
 rem ChangeColor 9 0
 rem Locate 9 35
 rem PrintColor 1)  14 %BGcol%
-rem PrintColor Track 7 %BGcol%
+rem PrintColor Track 11 %BGcol%
 rem Locate 11 35 
 rem PrintColor 2)  14 %BGcol%
-rem PrintColor Artist 7 %BGcol%
+rem PrintColor Artist 11 %BGcol%
 rem Locate 13 35
 rem PrintColor 3)  14 %BGcol%
-rem PrintColor Album 7 %BGcol%
+rem PrintColor Album 11 %BGcol%
 rem Locate 15 35
 rem PrintColor 4)  14 %BGcol%
-rem PrintColor Playlist 7 %BGcol%
-rem Locate 17 35
-rem PrintColor 5)  14 %BGcol%
-rem PrintColor Menu 7 %BGcol%
-rem PrintCenter Click on option! 21 8 %BGcol%
+rem PrintColor Playlist 11 %BGcol%
+rem Locate 21 28
+rem PrintColor BACK 12 0
+rem Locate 21 38
+rem PrintColor MENU 14 0
 rem ChangeColor 10 %BGcol%
-rem MouseCMD 32,9,48,9 32,11,48,11 32,13,48,13 32,15,48,15 32,17,48,17 29,17,29,17
+rem MouseCMD 35,9,41,9 35,11,42,11 35,13,41,13 35,15,44,15 28,21,33,21 38,21,42,21
 if %result%==1 goto soundcloudtrack
 if %result%==2 goto soundcloudartist
 if %result%==3 goto soundcloudalbum
 if %result%==4 goto soundcloudplaylist
 if %result%==5 goto Menu
-REM Keep Menu if invalid input
+if %result%==6 goto Menu
 goto soundcloud
 
-REM :soundcloud
-REM cls
-REM ECHO.
-REM ECHO !ESC![33m1) !WHITE!Track
-REM ECHO !ESC![33m2) !WHITE!Artist
-REM ECHO !ESC![33m3) !WHITE!Album
-REM ECHO !ESC![33m4) !WHITE!Playlist
-REM ECHO !ESC![33m5) !WHITE!Menu
-REM ECHO.
-REM SET /P option="!ESC![90mSelect option:!ESC![33m "
-REM if %option% == 1 (goto soundcloudtrack)
-REM if %option% == 2 (goto soundcloudartist)
-REM if %option% == 3 (goto soundcloudalbum)
-REM if %option% == 4 (goto soundcloudplaylist)
-REM if %option% == 5 (goto Menu)
-REM ECHO.
-REM ECHO !RED!Unknown value
-REM ECHO.
-REM goto soundcloud
 
 :soundcloudtrack
 cls
@@ -721,12 +525,3 @@ ECHO.
 ECHO !ESC![32mDone!
 timeout 2 >nul
 goto Menu
-
-REM playlist_id (string): Identifier of the playlist that contains the video
-REM playlist_title (string): Name of the playlist that contains the video
-REM playlist (string): playlist_id or playlist_title
-REM playlist_count (numeric): Total number of items in the playlist. May not be known if entire playlist is not extracted
-REM playlist_index (numeric): Index of the video in the playlist padded with leading zeros according the final index
-REM playlist_autonumber (numeric): Position of the video in the playlist download queue padded with leading zeros according to the total length of the playlist
-REM playlist_uploader (string): Full name of the playlist uploader
-REM playlist_uploader_id (string): Nickname or id of the playlist uploader
